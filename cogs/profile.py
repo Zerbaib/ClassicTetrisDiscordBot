df39f1636c0fc92b0a54059738109a0dfee1f99c
Zerbaib <hugo.hennetin@proton.me>
2023-11-06 02:56:51 +0100

    Add user profile creation functionality

--- cogs/profile.py
+++ cogs/profile.py
@@ -27,20 +27,56 @@ class ProfileCog(commands.Cog):
     
     @commands.slash_command(name="setprofile", description="Sets user profile")
     async def setprofile(self, ctx):
-        self.load_data()
+        data = self.load_data()
         user = ctx.author
+        
+        if user.id in data:
+            embed = disnake.Embed(
+                title="You already have a profile",
+                description="You can use `/profile` to see your profile",
+                color=disnake.Color.brand_red()
+            )
+            embed.set_footer(icon_url=user.avatar.url, text=f"Ececuted by {user}")
+            await ctx.send(embed=embed)
+            return
+        
         stats = {
-            "USERID": user.id,
-            "USERNAME": user.name,
-            "AVATAR": user.avatar.url,
-            "PLAYSTYLE": None,
-            "COUNTRY": None,
-            "SAMEPIECESET": None,
-            "NTSC": None,
-            "NTSC (19)": None,
-            "PAL": None,
-        }
-        self.save_data(stats)
+                "USERNAME": user.name,
+                "AVATAR": user.avatar.url,
+                "PLAYSTYLE": None,
+                "COUNTRY": None,
+                "SAMEPIECESET": None,
+                "GAME": {
+                    "CLASSIC": {
+                        "NTSC": None,
+                        "NTSC (19)": None,
+                        "PAL": None,
+                        "PAL (19)": None
+                    },
+                    "TETRISGB": {
+                        "NTSC": None,
+                        "NTSC (19)": None,
+                        "PAL": None,
+                        "PAL (19)": None
+                    },
+                    "TETRISN64": {
+                        "NTSC": None,
+                        "SPRINT": None,
+                        "MARATHON": None,
+                        "ULTRA": None
+                    },
+                    "TETRISDS": {
+                        "NTSC": None,
+                        "NTSC (19)": None,
+                        "PAL": None,
+                        "PAL (19)": None
+                    }
+                    }
+                }
+        
+        data[user.id] = stats
+        self.save_data(data)
+
         embed = disnake.Embed(
             title="Your profile has been set",
             description=f"You can now use `/profile` to see your profile\nTo set values, use `/setvalue <value> <value>`",
@@ -48,7 +84,6 @@ class ProfileCog(commands.Cog):
         )
         embed.set_footer(icon_url=user.avatar.url, text=f"Ececuted by {user}")
         await ctx.send(embed=embed)
-        pass
 
 def setup(bot):
     bot.add_cog(ProfileCog(bot))
