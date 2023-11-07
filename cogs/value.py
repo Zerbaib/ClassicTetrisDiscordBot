2be39e0504a06ecaf8fc3be45242764f47ed20c5
Zerbaib <hugo.hennetin@proton.me>
2023-11-07 15:30:50 +0100

    Add error handling to setvalue command

--- cogs/value.py
+++ cogs/value.py
@@ -18,36 +18,39 @@ class ValueCog(commands.Cog):
 
     @commands.slash_command(name="setvalue", description="Sets user value")
     async def setvalue(self, ctx, game: str = None, mode: str = None, value=int):
-        if game is None:
-            game = "CLASSIC"
-        elif mode is None:
-            mode = "NTSC"
-        else:
-            game = game.upper()
-            mode = mode.upper()
-        
-        if game not in ["CLASSIC", "TETRISGB", "TETRISN64", "TETRISDS"]:
+        try:
+            if game is None:
+                game = "CLASSIC"
+            elif mode is None:
+                mode = "NTSC"
+            else:
+                game = game.upper()
+                mode = mode.upper()
+            
+            if game not in ["CLASSIC", "TETRISGB", "TETRISN64", "TETRISDS"]:
+                embed = disnake.Embed(
+                    title="Invalid game",
+                    description="This game doesn't exist in the database\n\n**Available games:**\n- Classic\n- TetrisGB\n- TetrisN64\n- TetrisDS",
+                    color=disnake.Color.brand_red()
+                )
+                await ctx.send(embed=embed)
+                return
+            if mode not in self.data[ctx.author.id]['GAME'][game]:
+                ctx.send("Invalid mode")
+                return
+            
+            self.data[ctx.author.id]['GAME'][game][mode] = value
+            with open(self.data_file, "w") as f:
+                json.dump(self.data, f, indent=4)
+            
             embed = disnake.Embed(
-                title="Invalid game",
-                description="This game doesn't exist in the database\n\n**Available games:**\n- Classic\n- TetrisGB\n- TetrisN64\n- TetrisDS",
-                color=disnake.Color.brand_red()
+                title="Value set",
+                description=f"**Game:** {game}\n**Mode:** {mode}\n**Value:** {value}",
+                color=disnake.Color.brand_green()
             )
             await ctx.send(embed=embed)
-            return
-        if mode not in self.data[ctx.author.id]['GAME'][game]:
-            ctx.send("Invalid mode")
-            return
-        
-        self.data[ctx.author.id]['GAME'][game][mode] = value
-        with open(self.data_file, "w") as f:
-            json.dump(self.data, f, indent=4)
-        
-        embed = disnake.Embed(
-            title="Value set",
-            description=f"**Game:** {game}\n**Mode:** {mode}\n**Value:** {value}",
-            color=disnake.Color.brand_green()
-        )
-        await ctx.send(embed=embed)
+        except Exception as e:
+            await ctx.send(f"Error:\n\n```{e}```")
 
 def setup(bot):
     bot.add_cog(ValueCog(bot))
